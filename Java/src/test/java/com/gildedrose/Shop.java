5436ce2d60bd24b879bb7366350620898127c7ad
ClementLvl <77326086+ClementLvl@users.noreply.github.com>
2021-11-30 14:16:04 +0100

    Create Shop.java

---
+++ Java/src/test/java/com/gildedrose/Shop.java
@@ -0,0 +1,14 @@
+????
+  
+  CLASS Shop
+  
+  public IList<Item> Items get ; private set;}
+
+public Shop(iLIST<Item> itemsà
+  this.items=items;
+            
+            
+     void updateQuality()
+            foreach(Item i in this.items
+                i.SellIn--
+                i.Quality--
